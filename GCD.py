fd0d816a6350d68e6195d1bb4b2a9804a783d7dd
Mr Tech <koechfestus003@gmail.com>
2025-01-17 11:44:45 +0300

    gcd problem

--- GCD.py
+++ GCD.py
@@ -1,2 +1,17 @@
 from math import gcd
-def gcdOfString():
+def gcdOfString(str1, str2):
+    # check if str1 + str2= str2+str1
+    if str1 + str2 != str2 + str1:
+        return ""
+    
+    # find the gcd of the length of the strings
+    gcd_length = gcd(len(str1), len(str2))
+
+    # the gcd string is the prefix of str1 with length equal to gcd_length
+    return str1[:gcd_length]
+
+# usage
+print(gcdOfString("ABCABC", "ABC"))
+print(gcdOfString("ABABAB", "ABAB"))
+print(gcdOfString("ME", "FESTUS"))
+    
